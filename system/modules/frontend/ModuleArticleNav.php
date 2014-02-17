51a6f0496c686d90d8ffd1eca67389f6b2bc4022
Christian Schiffler <c.schiffler@cyberspectrum.de>
2014-02-17 19:15:35 +0100

    Backported the fix for the potential XSS vulnerability from 2.10.2.

--- system/modules/frontend/ModuleArticleNav.php
+++ system/modules/frontend/ModuleArticleNav.php
@@ -90,8 +90,8 @@ class ModuleArticleNav extends Module
 			{
 				return '';
 			}
- 
-			$strAlias = (strlen($this->objArticles->alias) && !$GLOBALS['TL_CONFIG']['disableAlias']) ? $this->objArticles->alias : $this->objArticles->id;
+
+			$strAlias = ($this->objArticles->alias != '' && !$GLOBALS['TL_CONFIG']['disableAlias']) ? $this->objArticles->alias : $this->objArticles->id;
 			$this->redirect($this->addToUrl('articles=' . $strAlias));
 		}
 
@@ -195,5 +195,3 @@ class ModuleArticleNav extends Module
 		}
 	}
 }
-
-?>
