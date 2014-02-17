356c1d31494f78cf433ba4a1081d8a8f8870c09c
Tristan Lins <tristan.lins@bit3.de>
2014-02-17 20:51:22 +0100

    Backported the fix for the potential XSS vulnerability from 2.10.2.

--- system/constants.php
+++ system/constants.php
@@ -45,7 +45,7 @@ define('URL_SUFFIX', '.html');
 define('ENCODE_AMPERSANDS', true);
 define('SWITCH_ORDER', true);
 define('VERSION', '2.7');
-define('BUILD', '7-cca-soa-hotfix2');
+define('BUILD', '7-cca-soa-hotfix3');
 
 
 /**
