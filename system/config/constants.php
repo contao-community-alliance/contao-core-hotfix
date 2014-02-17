ed8f407f46788a23a5a04fa528f76c40faceb561
Tristan Lins <tristan.lins@bit3.de>
2014-02-17 23:33:39 +0100

    Change the version naming scheme.

--- system/config/constants.php
+++ system/config/constants.php
@@ -18,7 +18,7 @@
  * the web browser reload those resources after a Contao update.
  */
 define('VERSION', '3.0');
-define('BUILD', '6-cca-soa-hotfix2');
+define('BUILD', '6-cca.soa.soa-hardening');
 define('LONG_TERM_SUPPORT', false);
 define('CODEMIRROR', '2.32');
 define('CSS3PIE', '1.0.0');
